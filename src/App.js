bab55a3f5a9586d2120d23034cb5617c93fa8e6d
Anji515 <anji.gd17@gmail.com>
2023-03-15 22:19:14 +0530

    Added Navbar and Footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
+import Home from './Components/Home';
 
 
 function App() {
   return (
     <>
       <Navbar />
+      <br/>
+      <br/>
+      <Home/>
       <br/>
       <br/>
       <Footer />
---
+++ src/Components/Home.jsx
@@ -0,0 +1,36 @@
+import {Button,Flex,Heading,Image,Stack,Text} from '@chakra-ui/react';
+  import { DownloadIcon } from '@chakra-ui/icons'
+  import { Link } from "react-scroll";
+
+
+//   const handleDownloadResume = ()=>{ }
+
+
+  export default function Home() {
+    return (
+        <Stack bg={'#c1cbd5'}>
+            <Heading
+              textAlign={{ base: "center", md: "left", lg: "left" }}
+              fontSize={{ base: "2xl", md: "3xl", lg: "4xl" }}
+            >
+              <Text color={"blue.600"} as={"span"}>
+                Hi 👋, I'am
+                <br />
+                Kommu Anjaneyulu
+              </Text>
+            </Heading>
+            <Text
+              fontSize={{ base: "md", lg: "lg" }}
+              color={"gray.800"}
+              textAlign={{ base: "left", md: "left", lg: "left" }}
+            >
+              Detailed-oriented and responsible Full-Stack Web Developer with a
+              great knowledge of Web Development. Capable of writing efficient
+              code using ReactJS, CSS, NodeJS and Express. Passionate about
+              coding and looking for an opportunity in software development.
+            </Text>
+
+
+        </Stack>
+    );
+  }
